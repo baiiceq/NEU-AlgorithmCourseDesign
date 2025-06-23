f0014bffb5fe37c4e360fd431009c37656306586
wzyzsy <2579351233@qq.com>
2025-06-23 09:23:13 +0800

    添加金币与陷阱类

--- tile.h
+++ tile.h
@@ -30,3 +30,27 @@ public:
 private:
     TileType type;
 };
+
+class Gold : public Tile
+{
+public:
+	Gold(int value = 5) : Tile(TileType::Gold), value(value) {}
+
+	int get_value() const { return value; }
+	void set_value(int new_value) { value = new_value; }
+
+private:
+	int value;  // 金币的价值
+};
+
+class Trap : public Tile
+{
+public:
+	Trap(int damage = 10) : Tile(TileType::Trap), damage(damage) {}
+
+	int get_damage() const { return damage; }
+	void set_damage(int new_damage) { damage = new_damage; }
+
+private:
+	int damage;  // 陷阱造成的伤害
+};
