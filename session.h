5ec069803199f75ab2df9106a703c085d07cbc65
Stephen <stephenzhang0529@hotmail.com>
2025-06-26 21:41:18 +0800

    fixing bugs
    Signed-off-by: Stephen <stephenzhang0529@hotmail.com>

--- session.h
+++ session.h
@@ -11,8 +11,8 @@ public:
     // 构造函数
     CrackingSession() = default;
 
-    // 运行整个破译会话的主方法
-    void run();
+    // 运行整个破译会话的主方法，接收一个目标密码
+    void run(const std::string& target_password);
 
 
     // 获取由用户密码生成的SHA-256哈希值
@@ -22,11 +22,11 @@ public:
 private:
     // --- 私有成员变量 ---
     std::string originalPassword_;
-    std::string targetHash_;
     std::vector<std::vector<int>> clues_;
 
     // --- 私有辅助方法 ---
-    void setupTargetPassword();
+    // 接收一个密码并进行设置
+    void setupTargetPassword(const std::string& password_to_set);
     void gatherClues();
     void executeAndReport();
     static std::vector<int> parseClue(const std::string& line);
